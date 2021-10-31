16aa2762a44d05e84b3c47730f49be6c157f92da
KentaSugaya <a1130ken@gmail.com>
2021-10-31 15:15:42 +0900

    アナログ時計を表示

--- javascript/main.js
+++ javascript/main.js
@@ -3,4 +3,6 @@ const date = new Date();
 const year = date.getFullYear(); // 年
 
 // HTMLに表示
-console.log(year);
+console.log(year)
+document.write(year)
+document.querySelectorAll("log").innerHTML = '$今年は{year}年です';
